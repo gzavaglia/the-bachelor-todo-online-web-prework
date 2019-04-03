6a6df1dee2bb53136ad00698b004183e7ed62e24
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-03 02:46:11 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,13 @@
 require 'pry'
 def get_first_name_of_season_winner(data, season)
-season_data = data[season]
+  season_data = data[season]
+  season_data.each do |contestant|
+    contestant.each do |key, value|
+  
+    end #contestant do 
+  end # end season do
 binding.pry
-end
+end #end get_first_name_of_season_winner
 
 def get_contestant_name(data, occupation)
   # code here
