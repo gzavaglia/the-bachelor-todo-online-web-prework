80e5a68ccdd7c5f3adfc765d109f648a9f86ae5e
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-03 03:16:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,8 +50,6 @@ def get_occupation(data, hometown)
    attributes.each do |contestant|
      contestant.each do |key, value|
        if value == hometown
-         name = contestant["name"]
-         seasons = season
          occupation = contestant["occupation"]
          binding.pry 
        end #if 
